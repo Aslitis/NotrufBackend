6b6e743c1c420f4f445c00609ed05aa8c7b31c2e
Asl <96251251+Aslitis@users.noreply.github.com>
2022-06-29 17:57:42 +0200

    Security classes for Login
    User and Roles for basic authentication and authorisation

---
+++ src/main/java/htw/projektarbeit/webApplication/SecurityConfiguration.java
@@ -0,0 +1,11 @@
+package htw.projektarbeit.webApplication;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+//Fuer Nutzeranmeldung
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    
+    
+}
